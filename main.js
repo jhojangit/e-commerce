e0b8b87e48c01df81c11d9e88cfc9fb828457f47
jhojangit <jhojanblackout@gmail.com>
2023-03-12 20:37:41 -0500

    add product

--- main.js
+++ main.js
@@ -1,7 +1,40 @@
 
 async function main(){
-    let res = JSON.parse(window.localStorage.getItem('products')) || await getProducts()
-    showProducts(res)
+    const db = {
+        products: JSON.parse(window.localStorage.getItem('products')) 
+                    || await getProducts(),
+
+        cart: {}
+    }
+
+
+
+    showProducts(db)
+
+    const productsHTML= document.querySelector('.main_products')
+    productsHTML.addEventListener('click', e=> {
+        if(e.target.classList.contains('product_button')){
+            const productID = Number(e.target.id)
+
+            const productFound = db.products.find(product => product.id === productID)
+
+        if(db.cart[productFound.id]){
+            db.cart[productFound.id].amount ++
+        }else{
+            let newProduct = structuredClone(productFound)
+            newProduct.amount = 1
+            db.cart[productFound.id] = newProduct
+
+        }
+
+        console.log(db.cart);
+
+
+        } 
+        })
+
+        
+
 }
 
 async function getProducts() {
@@ -11,25 +44,25 @@ async function getProducts() {
     return res;
 }
 
-function showProducts(products) {
+function showProducts(db) {
     let htmlProducts = document.querySelector('.main_products')
     let html = ''
 
 
-    for (const product in products) {
+    for (const i in db.products) {
         html += `
         <div class="product_container">
-            <img src="${products[product].image}" alt="">
+            <img src="${db.products[i].image}" alt="">
             <div class="product_line">
-                <p class="product_price">$${products[product].price} <span>Stock ${products[product].quantity}</span></p>
-                <button class="product_button" id="${product}">+</button>
+                <p class="product_price">$${db.products[i].price} <span>Stock ${db.products[i].quantity}</span></p>
+                <button class="product_button" id="${db.products[i].id}">+</button>
                 </div>
-            <p class="product_name">${products[product].name}</p>
+            <p class="product_name">${db.products[i].name}</p>
         </div>`
 
     }
     htmlProducts.innerHTML = html
-    
+
 }
 
 main()
